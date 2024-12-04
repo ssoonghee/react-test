3483c524bd5377211dadd7c850d58e58403f501d
Songhee Yun <thdgml1011@mailplug.co.kr>
2024-12-04 18:29:13 +0900

    🧹 chore: 불필요한 파일 제거

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import './output.css';
 
 function App() {
   return (
--- front/src/index.tsx
+++ front/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import './css/output.css';
+import './input.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
