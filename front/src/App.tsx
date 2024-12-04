8c7f5bcc5f6787424f078635fea1bf4736e3f13b
Songhee Yun <thdgml1011@mailplug.co.kr>
2024-12-04 16:09:57 +0900

    🚀 feat: React typrscript 템플릿 설치

---
+++ front/src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './output.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p className='absolute bg-red-400 h-8'>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
